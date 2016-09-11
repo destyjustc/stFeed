9fd3c2e1cebf6d64a4d7955ff0822fb536271fee
user <user@egmail.com>
2016-09-11 13:44:13 -0700

    add interval

--- getData.py
+++ getData.py
@@ -5,9 +5,10 @@ from pprint import pprint
 import json
 from os import listdir, walk
 import time
+import threading
 
 def checkFile():
-	prefix = time.strftime('%Y_%m_%d')
+	prefix = time.strftime('%Y_%m_%d_%H_%M_%S')
 	exit = False
 	for filename in listdir('./files'):
 		if filename == prefix:
@@ -17,8 +18,7 @@ def checkFile():
 		f = open('./files/'+prefix, 'w')
 		return f
 
-
-def init():
+def getData():
 	s_list = ['GOOG', 'AAPL']
 	url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22YHOO%22%2C%22AAPL%22%2C%22GOOG%22%2C%22MSFT%22)%0A%09%09&format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env&callback="
 	f = checkFile()
@@ -26,5 +26,9 @@ def init():
 		data = requests.get(url)
 		json.dump(data.json(), f)
 
+def init(interval):
+	threading.Timer(interval, init, [interval]).start()
+	getData()
+
 if __name__ == "__main__":
-	init()
+	init(3)
