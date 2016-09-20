6a18455d64a7fe0fa532a9b3c83d6a1245dff6b0
Yong Zhang <michalezhang917@gmail.com>
2016-09-19 22:21:46 -0700

    correct the time

--- python/getData.py
+++ python/getData.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 
-from flask import Flask
-from flaskext.mysql import MySQL
+
 import requests
-from pprint import pprint
 import json
 from os import listdir, walk
 import time
@@ -11,7 +9,6 @@ import threading
 from pytz import timezone
 from datetime import datetime, timedelta
 import os
-import pandas as pd
 from pandas.tseries.holiday import USFederalHolidayCalendar as calendar
 
 def checkfolder(folder):
@@ -46,7 +43,7 @@ def getData(stockList):
 			query += i + '%22%2C%22'
 		query = query[0:-9];
 		query += sufix
-			# url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22YHOO%22%2C%22AAPL%22%2C%22GOOG%22%2C%22MSFT%22)%0A%09%09&format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env&callback="
+		# url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22YHOO%22%2C%22AAPL%22%2C%22GOOG%22%2C%22MSFT%22)%0A%09%09&format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env&callback="
 		folder = pacific_time.strftime('%Y_%m_%d')
 		checkfolder(folder)
 		filename = folder + '/' + pacific_time.strftime('%Y_%m_%d_%H_%M_%S') + '_part' + str(j+1)
@@ -77,13 +74,10 @@ def init(interval, stockList):
 	pacific_time = datetime.now(timezone('US/Pacific'))
 	eastern_time = datetime.now(timezone('US/Eastern'))
 	timenow = eastern_time.hour + float(eastern_time.minute) / 60
-	if not(datetime.now().date() in holidays) and (datetime.now().isoweekday() in range(1, 6)) and timenow >= 0 and timenow <= 24:
+	if not(datetime.now().date() in holidays) and (datetime.now().isoweekday() in range(1, 6)) and timenow >= 9 and timenow <= 17:
 		print "Get data"
 		print time.strftime("%Y-%m-%d %H:%M:%S %Z%z", time.gmtime())
-		fmt = "%Y-%m-%d %H:%M:%S %Z%z"
-		pacific_time = datetime.now(timezone('US/Pacific'))
 		print pacific_time.strftime(fmt)
-		eastern_time = datetime.now(timezone('US/Eastern'))
 		print eastern_time.strftime(fmt)
 		getData(stockList)
 	else:
