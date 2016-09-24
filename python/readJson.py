d6603469cbf09db5f8ce949066591d5febe4e47f
yong.zhang <yong.zhang@istuary.com>
2016-09-23 22:27:16 -0700

    read data

--- python/readJson.py
+++ python/readJson.py
@@ -3,24 +3,34 @@ import os, glob
 import urllib
 import csv
 from pprint import pprint
-
+import matplotlib.pyplot as plt
 prefix = '../data/'
-folder_list = os.listdir(prefix)
+mtime = lambda f: os.stat(os.path.join(prefix, f)).st_mtime
 
+folder_list =  list(sorted(os.listdir(prefix), key=mtime))
+ask_list = list()
+bid_list = list()
+time_list = list()
 for folder in folder_list :
     if os.path.isdir(prefix+folder) :
-        print folder
-        timepoint_list = os.listdir('../data/' + folder + '/')
-        for timepoint in timepoint_list :
-            if not timepoint.startswith('.') and timepoint.endswith('1') :
-                file_list = glob.glob(prefix + folder + '/' + timepoint[:-1] +'*')
+        path = '../data/' + folder + '/NASDAQ/' + folder
+        timepoint_list = sorted(glob.glob(path + "*1" ))
+        for timepoint in timepoint_list:
+            file_list = glob.glob(timepoint[:-1] +'*')
                 #print prefix + folder + '/' + file[:-1] +'*'
-                for file in file_list:
-                    with open(file) as data_file:
-                        data = json.load(data_file)
-                        #pprint(data["query"]["results"]["quote"][0])
-                        if data["query"]["results"]["quote"][0]["symbol"] == 'GOOG' :
-                            pprint(data["query"]["results"]["quote"][0])
+            for file in file_list:
+                with open(file) as data_file:
+                    data = json.load(data_file)["query"]["results"]["quote"]
+                    for i in range(0, len(data)):
+                        if data[i]["symbol"] == "AAPL" :
+                          #  pprint(data[i])
+                            pprint('Ask: ' + data[i]['Ask'] + ' Bid: ' + data[i]['Bid'] + ' @ ' + file[26:45])
+                            #pprint(data[i]['LastTradeWithTime'])
+                            ask_list.append(data[i]['Ask'])
+                            bid_list.append(data[i]['Bid'])
+                            time_list.append(file[26:45])
+
+
 
         #    for i in range(0,1) :
          #       pprint(data["query"]["results"]["quote"][i]['symbol']+':' + data["query"]["results"]["quote"][i]['symbol'])
